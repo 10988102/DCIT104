2a3fc03422f8591aa876c4b40da46b5da206b42d
10988102 <102968401+10988102@users.noreply.github.com>
2022-08-08 17:42:09 +0000

    Add files via upload

---
+++ dcit assignment.c
@@ -0,0 +1,38 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // listing all integers to be used
+    // givennum id the number to be input
+    int num, a, giennum, sum - 0;
+
+    //Enter the end number of the sum of the sum of prime numbers
+    cout <<"Find the sum of prime number upto : ";
+    cin >> givennum;
+
+        for(num =2; num <=givemmum; num++) {
+                for(a = 2; a <= (num / 2); a++){
+                        if(num % a == 0) {
+                                a = num;
+                                 break;
+                        }
+                }
+        }
+
+        //Adding of number when prime
+        if(i != num) {
+                sum +=num;
+
+        }
+}
+cout << enl << "sum of prime number upto" <<givennum <<":" << sum;
+
+return 0
+//Saeed Hierah developed this program
+// ID:10988102
+
+
+
+
